d67fcb708a9fbf123bb2be72dd7c51202ba91320
asaf <asaf0604@gmail.com>
2024-04-28 23:24:28 +0300

    hhh

--- RUDP_API.c
+++ RUDP_API.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <netinet/in.h>
 #include "RUDP_API.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port){
-    // Allocate memory for the RUDP socket structure
     RUDP_Socket* build = malloc(sizeof(RUDP_Socket));
     if (build == NULL) {
         perror("Error allocating memory for socket structure");
         return NULL;
     }
     
-    // Create a UDP socket
-    build->socket_fd = socket(AF_INET, SOCK_DGRAM, 0); // 0 for the UDP protocol
+    build->socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (build->socket_fd == -1) {
         perror("Error opening socket");
-        free(build); // Free the allocated memory
+        free(build);
         return NULL;
     }
     
-    // Set whether the socket is a server or client
     build->isServer = isServer;
 
-    // If it's a server socket, bind it to the specified port
     if (isServer) {
         struct sockaddr_in server_addr;
         memset(&server_addr, 0, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on all available interfaces
-        server_addr.sin_port = htons(listen_port); // Set the port
+        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        server_addr.sin_port = htons(listen_port);
 
-        // Bind the socket
         if (bind(build->socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
             perror("Error binding socket");
             close(build->socket_fd);
@@ -46,8 +39,6 @@ RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port){
     return build;
 }
 
-
-// Tries to connect to the other side via RUDP to given IP and port. Returns 0 on failure and 1 on success. Fails if called when the socket is connected/set to server.
 RUDP_Socket* rudp_connect(RUDP_Socket* sockfd, const char *dest_ip, unsigned short int dest_port){
      struct sockaddr_in dest_addr;
     
@@ -56,7 +47,7 @@ RUDP_Socket* rudp_connect(RUDP_Socket* sockfd, const char *dest_ip, unsigned sho
     dest_addr.sin_port = htons(dest_port);
     dest_addr.sin_addr.s_addr = inet_addr(dest_ip);
     if (dest_addr.sin_addr.s_addr == INADDR_NONE) {
-        fprintf(stderr, "Invaild ip\n");
+        fprintf(stderr, "Invalid IP\n");
         return NULL; 
     }
 
@@ -68,7 +59,6 @@ RUDP_Socket* rudp_connect(RUDP_Socket* sockfd, const char *dest_ip, unsigned sho
     return sockfd;
 }
 
-// Accepts incoming connection request and completes the handshake, returns 0 on failure and 1 on success. Fails if called when the socket is connected/set to client.
 RUDP_Socket* rudp_accept(RUDP_Socket* sockfd) {
     if (!sockfd->isServer) {
         fprintf(stderr, "Error: Cannot accept on a client socket\n");
@@ -78,32 +68,26 @@ RUDP_Socket* rudp_accept(RUDP_Socket* sockfd) {
     struct sockaddr_in cli_addr;
     socklen_t len = sizeof(cli_addr);
     
-    // Receive a message to establish connection
     ssize_t bytes_received = recvfrom(sockfd->socket_fd, NULL, 0, 0, (struct sockaddr *)&cli_addr, &len);
     if (bytes_received == -1) {
         perror("Error receiving connection request");
         return NULL;
     }
 
-    // Create a new RUDP_Socket for the accepted connection
-    RUDP_Socket* new_sock = (RUDP_Socket*)malloc(sizeof(RUDP_Socket));
+    RUDP_Socket* new_sock = malloc(sizeof(RUDP_Socket));
     if (new_sock == NULL) {
         perror("Error allocating memory for new socket");
         return NULL;
     }
 
-    // Initialize the new socket parameters
-    new_sock->socket_fd = sockfd->socket_fd; // Reuse the same socket file descriptor
-    new_sock->isServer = false; // This socket is now a client
+    new_sock->socket_fd = sockfd->socket_fd;
+    new_sock->isServer = false;
     new_sock->isConnected = true;
     new_sock->dest_addr = cli_addr;
 
     return new_sock;
 }
 
-
-
-// Receives data from the other side and put it into the buffer. Returns the number of received bytes on success, 0 if got FIN packet (disconnect), and -1 on error. Fails if called when the socket is disconnected.
 ssize_t rudp_recv(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size) {
     if (!sockfd->isConnected) {
         fprintf(stderr, "Error: Socket is not connected\n");
@@ -115,7 +99,6 @@ ssize_t rudp_recv(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size) {
     
     ssize_t bytes_received = recvfrom(sockfd->socket_fd, buffer, buffer_size, 0, (struct sockaddr*)&sender_addr, &sender_addr_len);
     if (bytes_received == 0) {
-        // Got FIN packet (disconnect)
         sockfd->isConnected = false;
     } else if (bytes_received == -1) {
         perror("Error receiving data");
@@ -124,63 +107,48 @@ ssize_t rudp_recv(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size) {
     return bytes_received;
 }
 
-
-
-// Sends data stores in buffer to the other side. Returns the number of sent bytes on success, 0 if got FIN packet (disconnect), and -1 on error. Fails if called when the socket is disconnected.
 ssize_t rudp_send(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size){
     ssize_t total_bytes_sent = 0;
     ssize_t bytes_sent = 0;
     char *ptr = (char*)buffer;
 
-    // Keep sending until all data in the buffer is sent
     while (total_bytes_sent < buffer_size) {
-        // Attempt to send data
         bytes_sent = sendto(sockfd->socket_fd, ptr + total_bytes_sent, buffer_size - total_bytes_sent, 0,
                             (struct sockaddr *)&(sockfd->dest_addr), sizeof(sockfd->dest_addr));
         
-        // Check for errors
         if (bytes_sent == -1) {
             perror("Error sending data");
-            return -1; // Error sending data
+            return -1;
         } else if (bytes_sent == 0) {
             printf("FIN packet received.\n");
-            return 0; // FIN packet received
+            return 0;
         }
 
-        // Update the total number of bytes sent
         total_bytes_sent += bytes_sent;
     }
 
     return total_bytes_sent;
 }
 
-
-// Disconnects from an actively connected socket. Returns 1 on success, 0 when the socket is already disconnected (failure).
 RUDP_Socket* rudp_disconnect(RUDP_Socket* sockfd) {
-    // Disconnect only if the socket is currently connected
     if (sockfd->isConnected) {
         sockfd->isConnected = false;
-        return sockfd; // Success
+        return sockfd;
     }
-    return 0; // Already disconnected
+    return NULL;
 }
 
-// This function releases all the memory allocation and resources of the socket.
 RUDP_Socket* rudp_close(RUDP_Socket* sockfd) {
-    // Close the UDP socket
     close(sockfd->socket_fd);
     free(sockfd);
     return NULL;
 }
 
-
-// Function to listen for incoming connections on the RUDP socket
 int rudp_listen(RUDP_Socket* sockfd, int backlog) {
-    // Listen for incoming connections
     if (listen(sockfd->socket_fd, backlog) == -1) {
         perror("Error listening for connections");
         return -1;
     }
 
-    return 0; // Success
+    return 0;
 }
--- RUDP_API.h
+++ RUDP_API.h
@@ -1,44 +1,23 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <netinet/in.h>
 #ifndef RUDP_API_H
 #define RUDP_API_H
 
+#include <stdbool.h>
+#include <netinet/in.h>
 
-//A struct that represents RUDP Socket
-typedef struct _rudp_socket
-{
-int socket_fd; // UDP socket file descriptor
-bool isServer; // True if the RUDP socket acts like a server, false for client.
-bool isConnected; // True if there is an active connection, false otherwise.
-struct sockaddr_in dest_addr; // Destination address. Client fills it when it connects via rudp_connect(), server fills it when it accepts a connection via rudp_accept().
+typedef struct {
+    int socket_fd;
+    bool isServer;
+    bool isConnected;
+    struct sockaddr_in dest_addr;
 } RUDP_Socket;
 
-// Allocates a new structure for the RUDP socket (contains basic information about the socket itself). Also creates a UDP socket as a baseline for the RUDP. isServer means that this socket acts like a server. If set to server socket, it also binds the socket to a specific port.
 RUDP_Socket* rudp_socket(bool isServer, unsigned short int listen_port);
-
-// Tries to connect to the other side via RUDP to given IP and port. Returns 0 on failure and 1 on success. Fails if called when the socket is connected/set to server.
-RUDP_Socket* rudp_connect(RUDP_Socket *sockfd, const char *dest_ip, unsigned short int dest_port);
-
-// Accepts incoming connection request and completes the handshake, returns 0 on failure and 1 on success. Fails if called when the socket is connected/set to client.
-RUDP_Socket* rudp_accept(RUDP_Socket *sockfd);
-
-// Receives data from the other side and put it into the buffer. Returns the number of received bytes on success, 0 if got FIN packet (disconnect), and -1 on error. Fails if called when the socket is disconnected.
-ssize_t rudp_recv(RUDP_Socket *sockfd, void *buffer, unsigned int buffer_size);
-
-// Sends data stores in buffer to the other side. Returns the number of sent bytes on success, 0 if got FIN packet (disconnect), and -1 on error. Fails if called when the socket is disconnected.
-ssize_t rudp_send(RUDP_Socket *sockfd, void *buffer, unsigned int buffer_size);
-
-// Disconnects from an actively connected socket. Returns 1 on success, 0 when the socket is already disconnected (failure).
-RUDP_Socket* rudp_disconnect(RUDP_Socket *sockfd);
-
-// This function releases all the memory allocation and resources of the socket.
-RUDP_Socket* rudp_close(RUDP_Socket *sockfd);
-
-// Function to listen for incoming connections on the RUDP socket
+RUDP_Socket* rudp_connect(RUDP_Socket* sockfd, const char *dest_ip, unsigned short int dest_port);
+RUDP_Socket* rudp_accept(RUDP_Socket* sockfd);
+ssize_t rudp_recv(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size);
+ssize_t rudp_send(RUDP_Socket* sockfd, void *buffer, unsigned int buffer_size);
+RUDP_Socket* rudp_disconnect(RUDP_Socket* sockfd);
+RUDP_Socket* rudp_close(RUDP_Socket* sockfd);
 int rudp_listen(RUDP_Socket* sockfd, int backlog);
- 
-
-
 
 #endif /* RUDP_API_H */
